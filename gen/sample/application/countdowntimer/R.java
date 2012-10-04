2d50837f59e4990afa7a64fbe416835c64a9ba5f
gondoh <gondoh@gufii.net>
2012-10-04 21:47:54 +0900

    p156まで作成

--- gen/sample/application/countdowntimer/R.java
+++ gen/sample/application/countdowntimer/R.java
@@ -18,9 +18,17 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int thumb=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int buttonSettings=0x7f080003;
+        public static final int buttonStart=0x7f080005;
+        public static final int buttonStop=0x7f080006;
+        public static final int linearLayout1=0x7f080000;
+        public static final int linearLayout2=0x7f080004;
+        public static final int menu_settings=0x7f080007;
+        public static final int seekBar1=0x7f080002;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_countdown_timer=0x7f030000;
@@ -31,6 +39,9 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int label_settings=0x7f050006;
+        public static final int label_start=0x7f050004;
+        public static final int label_stop=0x7f050005;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_countdown_timer=0x7f050003;
     }
--- src/sample/application/countdowntimer/CountdownTimerActivity.java
+++ src/sample/application/countdowntimer/CountdownTimerActivity.java
@@ -1,17 +1,49 @@
 package sample.application.countdowntimer;
 
+import java.text.SimpleDateFormat;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
 public class CountdownTimerActivity extends Activity {
+	
+	public static TextView tv;
+	public static SeekBar sb;
+	public static Context mContext;
+	public static int timeLeft = 0;
+	public static Button btnStart;
+	public static Button btnStop;
+	
+	
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countdown_timer);
+        
+
+    	CountdownTimerActivity.mContext = this;
+    	CountdownTimerActivity.tv = (TextView)this.findViewById(R.id.textView1);
+    	CountdownTimerActivity.btnStart = (Button)this.findViewById(R.id.buttonStart);        
+    	CountdownTimerActivity.btnStop = (Button)this.findViewById(R.id.buttonStop);        
+        CountdownTimerActivity.sb = (SeekBar)this.findViewById(R.id.seekBar1);
+        CountdownTimerActivity.sb.setBackgroundDrawable(this.drowScale());
+        this.setListeners();
     }
 
     @Override
@@ -20,5 +52,80 @@ public class CountdownTimerActivity extends Activity {
         return true;
     }
 
+    public BitmapDrawable drowScale(){
+    	Paint paint;
+    	Path path;
+    	Canvas canvas;
+    	Bitmap bitmap;
+    	
+    	paint = new Paint();
+    	paint.setStrokeWidth(0);
+    	paint.setStyle(Paint.Style.STROKE);
+    	bitmap = Bitmap.createBitmap(241, 30, Bitmap.Config.ARGB_8888);
+    	path = new Path();
+    	canvas = new Canvas(bitmap);
+    	
+    	for(int i = 0; i < 17; i++){
+    		path.reset();
+    		if (i % 5 == 0){
+    			paint.setColor(Color.WHITE);
+    		} else {
+    			paint.setColor(Color.GRAY);
+    		}
+    		path.moveTo(i*16, 5);
+    		path.quadTo(i*16, 5, i*16, 15);
+    		canvas.drawPath(path, paint);
+    	}
+    	BitmapDrawable bd = new BitmapDrawable(bitmap);
+    	
+    	return bd;
+    }
+    
+    
+    
+    public void setListeners(){
+    	CountdownTimerActivity.sb.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				timeLeft = progress*60;
+				if (fromUser) showTime(progress*60);
+				if (fromUser && (progress > 0)) btnStart.setEnabled(true);
+				else 							btnStart.setEnabled(false);
+				if (progress == 0) btnStop.setEnabled(false);
+			}
+		});
+    }
+    
+    public static void showTime(int timeSeconds){
+    	SimpleDateFormat format = new SimpleDateFormat("mm:ss");
+    	CountdownTimerActivity.tv.setText(format.format(timeSeconds*1000));
+    }
+    
+    public static void countDown(int counter){
+    	showTime(counter);
+    	timeLeft = counter;
+    	if (counter % 60 == 0) sb.setProgress(counter/60);
+    	else     			   sb.setProgress(counter/60 + 1);
+    	
+    	if (counter != 0) {
+    		btnStop.setEnabled(true);
+    		btnStart.setEnabled(false);
+    		sb.setEnabled(false);
+    	} else {
+    		btnStop.setEnabled(false);
+    		btnStart.setEnabled(false);
+    		sb.setEnabled(true);
+    	}
+    }
     
 }
